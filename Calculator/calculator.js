48db4926cee5a6bbf566ec044fa9edf7d100ac61
MrNullPointer <58353903+MrNullPointer@users.noreply.github.com>
2020-12-02 12:36:27 -0500

    First Server

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
+  console.log(req);
   res.sendFile(__dirname + "/index.html"); //to send an entire webpage (index.html)
 });
 
@@ -20,6 +21,18 @@ app.post("/", function (req, res) {
   var result = num1 + num2;
 
   res.send("The result of calculation is: " + result);
+});
+
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function (req, res) {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+
+  var bmi = weight / (height * height);
+  res.send("Your BMI is: " + bmi);
 });
 
 app.listen(3000, function () {
